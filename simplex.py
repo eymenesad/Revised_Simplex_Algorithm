dd5aa4d1d3eb5b0d310d625de1c013642fc074ff
eymenesad <eymenesad123@gmail.com>
2023-12-03 17:49:55 +0300

    a

--- simplex.py
+++ simplex.py
@@ -1,25 +1,7 @@
+import numpy as np
 # Inverse of B matrix
 def modify_B(input_matrix):
-    n = len(input_matrix)
-    identity_matrix = [[0] * n for _ in range(n)]
-    for i in range(n):
-        identity_matrix[i][i] = 1
-
-    # Apply Gaussian elimination to calculate the inverse
-    for i in range(n):
-        pivot = input_matrix[i][i]
-        for j in range(n):
-            input_matrix[i][j] /= pivot
-            identity_matrix[i][j] /= pivot
-        for k in range(n):
-            if k != i:
-                factor = input_matrix[k][i]
-                for j in range(n):
-                    input_matrix[k][j] -= factor * input_matrix[i][j]
-                    identity_matrix[k][j] -= factor * identity_matrix[i][j]
-    print(identity_matrix)
-    return identity_matrix
-
+    return np.linalg.inv(input_matrix)
 
 # Vector multiplication: c_B * inverse_B
 def c_B_multiply_Bmatrix(c_B, inverse_B):
@@ -46,9 +28,7 @@ def initialize_tableau(c, normal_matrix):
         tableau[i].extend([0] * i)
         tableau[i].extend([1.0])
         tableau[i].extend([0] * (m - i - 1))
-        
-    negative_c = [-x for x in c]
-    tableau.insert(0, negative_c + [0]*m)
+  
     return tableau
 def c_B_multiply_Bmatrix(c_B, inverse_B):
     ans = [0 for t in range(m)]
@@ -58,113 +38,166 @@ def c_B_multiply_Bmatrix(c_B, inverse_B):
     return ans
 
 def find_entering_variable():
-    new_c = [0 for ii in range(n)]
+    new_c = dict()
     inverse_B = modify_B(B_matrix)
+
 
     c_B_inverseB = c_B_multiply_Bmatrix(c_B, inverse_B)
 
-    for i in range(n):
+    for i in range(mn):
         if i in basic_set:
             continue
         c_B_A_i = 0
         for j in range(m):
-            c_B_A_i += c_B_inverseB[j] * tableau[i][j]
+            c_B_A_i += c_B_inverseB[j] * tableau[j][i]
+            
         new_c[i] = c[i] - c_B_A_i
-    pivot_column = min(new_c)
-    if pivot_column >= 0:
+    
+    flag = 0
+    for element in new_c:
+
+        print(element, "new_c[element]: ", new_c[element])
+        if new_c[element] < 0:
+            flag = 1
+            break
+    if flag == 0:
         return None  # Optimal solution reached
-    return new_c.index(pivot_column), new_c
+
+    min_val = 100000000
+    for element in new_c:
+        if new_c[element] < min_val:
+            min_val = new_c[element]
+            min_index = element
+
+
+    return min_index
+        
 
 def find_leaving_variable(entering_variable):
+
+    inverse_B = modify_B(B_matrix)
+
+    column_star = [0 for i in range(m)]
+    for h in range(m):
+        for l in range(m):
+            column_star[h] += tableau[l][entering_variable] * inverse_B[h][l]
+
+    b_star = [0 for i in range(m)]
+    for h in range(m):
+        for l in range(m):
+            b_star[h] += b[l] * inverse_B[h][l]
+
     ratios = []
     for r in range(m):
-        if tableau[r][entering_variable] > 0:
-            ratios.append((b[r] / tableau[r][entering_variable], r))
+        if column_star[r] == 0:
+            ratios.append(10000000)
+            continue
+        val = b_star[r] / column_star[r]
+        if val>0:
+            ratios.append(val)
+        else:
+            ratios.append(10000000)
 
     if not ratios:
-        return None  # Problem is unbounded
-    min_ratio, leaving_variable_row = min(ratios, key=lambda x: x[0])
-    return leaving_variable_row
+        return None  # Problem is unbounded     
+
+    min_ratio = min(ratios)
+    return ratios.index(min_ratio)
 
 def update_tableau(entering_variable, leaving_variable):
 
 
-    basic_set.append(entering_variable)
-    basic_set.remove(leaving_variable)
+    basic_set[leaving_variable] = entering_variable
+    #basic_set.remove(basic_set[leaving_variable])
 
-    B_matrix = []
-    for i in range(m):
+    
+    
+    B_matrix.clear()
+    for basic in basic_set:
         arr=[]
-        for basic in basic_set:
+        for i in range(m):
             arr.append(tableau[i][basic])
         B_matrix.append(arr)
 
-    c_B = []
+
+    c_B.clear()
     for basic in basic_set:
         c_B.append(c[basic])
 
-    # tableu = inverse_of_B * tableau
-    inverse_B = modify_B(B_matrix)
-    for i in range(m):
-        for j in range(n):
-            tableau[i][j] = 0
-            for k in range(m):
-                tableau[i][j] += inverse_B[i][k] * B_matrix[k][j]
+
 
 
 
 def revised_simplex_algorithm():
+    sas = 0
     while True:
+
+        print("basic_set: ", basic_set)
         
-        entering_variable, c = find_entering_variable()
+        entering_variable = find_entering_variable()
         if entering_variable is None:
             break  # Optimal solution reached
-        leaving_variable = find_leaving_variable(tableau, entering_variable)
+        leaving_variable = find_leaving_variable(entering_variable)
         if leaving_variable is None:
             print("Unbounded problem.")
             return
         update_tableau(entering_variable, leaving_variable)
+        sas += 1
+        if sas == 100:
+            return
 
-    optimal_solution = [0] * (len(tableau[0]) - 1)
-    for i in range(len(optimal_solution)):
-        column = [row[i] for row in tableau[1:]]
-        if column.count(0) == len(column) - 1 and column[-1] == 1:
-            pivot_row = column.index(1)
-            optimal_solution[i] = tableau[pivot_row + 1][-1]
-
-    optimal_result = sum(x * y for x, y in zip(c, optimal_solution))
 
     print("Hence, optimal solution is arrived with value of variables as :")
-    for i, value in enumerate(optimal_solution):
-        print(f"x{i + 1} = {value}", end=", ")
+    
+    print("b:", b)
+    print("B_matrix:", B_matrix)
 
-    print(f"\nMax Z = {optimal_result}")
+    inverse_B = modify_B(B_matrix)
+    b_star_optimum = [0 for i in range(m)]
+    for h in range(m):
+        for l in range(m):
+            b_star_optimum[h] += b[l] * inverse_B[l][h]
+
+    print("b_star_optimum:", b_star_optimum)
+    
+    for y in range(m):
+        element = basic_set[y]
+        if element >= n:
+            continue
+        print(f"x{element+1} = {b_star_optimum[y]}")
+
+
+
 
 
 if __name__ == '__main__':
     for m in range(1, 4):
+        if m != 3:
+            continue
         print(f"Output for case {m}:")
         m, n, c, augmented_matrix = read_matrix(f"Assignment3_Data{m}.txt")
+        mn = m+n
         b = [row[-1] for row in augmented_matrix]
+        c = [-x for x in c]
+        c = c + [0]*m
         normal_matrix = [row[:-1] for row in augmented_matrix]
         #print(f"normal matrix {normal_matrix}")
-        n = n+m
+        #n = n+m
 
 
         tableau = initialize_tableau(c, normal_matrix)
-        print(f"tableau {tableau}")
         basic_set = [f for f in range(n, n+m)]
-        print(f"basic_set {basic_set}")
+        
         B_matrix = []
         for i in range(m):
             arr=[]
             for basic in basic_set:
                 arr.append(tableau[i][basic])
             B_matrix.append(arr)
-
         c_B = []
         for basic in basic_set:
             c_B.append(c[basic])
+
         revised_simplex_algorithm()
         print("-" * 20)
-        break
+        
