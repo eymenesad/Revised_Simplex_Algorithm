f8124f7e7b73cbf0ba582ae2b816b5d5899a5064
eymenesad <eymenesad123@gmail.com>
2023-12-03 13:00:11 +0300

    s

--- simplex.py
+++ simplex.py
@@ -1,54 +1,145 @@
+import numpy as np
+
+
+#inverse of B matrix
+def modify_B(input_matrix):
+    return np.linalg.inv(input_matrix)
+
+
 def read_matrix(filename):
     with open(filename, 'r') as file:
         m, n = map(int, file.readline().split())
         c = list(map(float, file.readline().split()))
         augmented_matrix = [list(map(float, line.split())) for line in file]
-
     return m, n, c, augmented_matrix
 
 def print_matrix(matrix):
     for row in matrix:
         print(' '.join(map(str, row)))
 
-def simplex_algorithm(m, n, c, augmented_matrix):
-    # Phase 1: Convert the problem to standard form
+def initialize_tableau(c, normal_matrix):
+    tableau = [row for row in normal_matrix]
     for i in range(m):
-        if augmented_matrix[i][n] < 0:
-            for j in range(n + 1):
-                augmented_matrix[i][j] *= -1
+        tableau[i].extend([0] * i)
+        tableau[i].extend([1.0])
+        tableau[i].extend([0] * (m - i - 1))
+        
+    negative_c = [-x for x in c]
+    tableau.insert(0, negative_c + [0]*m)
+    return tableau
+def c_B_multiply_Bmatrix(c_B, inverse_B):
+    ans = [0 for t in range(m)]
+    for i in range(m):
+        for j in range(m):
+            ans[i] += c_B[j] * inverse_B[j][i]
+    return ans
 
-    # Phase 2: Apply the simplex algorithm
-    while any(x < 0 for x in augmented_matrix[m][:n]):
-        pivot_column = augmented_matrix[m].index(min(augmented_matrix[m][:n]))
+def find_entering_variable():
+    new_c = [0 for ii in range(n)]
+    inverse_B = modify_B(B_matrix)
 
-        ratios = [(augmented_matrix[i][n] / augmented_matrix[i][pivot_column], i) for i in range(m) if augmented_matrix[i][pivot_column] > 0]
-        pivot_row = min(ratios)[1]
+    c_B_inverseB = c_B_multiply_Bmatrix(c_B, inverse_B)
 
-        pivot_element = augmented_matrix[pivot_row][pivot_column]
+    for i in range(n):
+        if i in basic_set:
+            continue
+        c_B_A_i = 0
+        for j in range(m):
+            c_B_A_i += c_B_inverseB[j] * tableau[i][j]
+        new_c[i] = c[i] - c_B_A_i
+    pivot_column = min(new_c)
+    if pivot_column >= 0:
+        return None  # Optimal solution reached
+    return new_c.index(pivot_column), new_c
 
-        # Update the tableau
-        for j in range(n + 1):
-            augmented_matrix[pivot_row][j] /= pivot_element
+def find_leaving_variable(entering_variable):
+    ratios = []
+    for r in range(m):
+        if tableau[r][entering_variable] > 0:
+            ratios.append((b[r] / tableau[r][entering_variable], r))
 
-        for i in range(m + 1):
-            if i != pivot_row:
-                factor = augmented_matrix[i][pivot_column]
-                for j in range(n + 1):
-                    augmented_matrix[i][j] -= factor * augmented_matrix[pivot_row][j]
+    if not ratios:
+        return None  # Problem is unbounded
+    min_ratio, leaving_variable_row = min(ratios, key=lambda x: x[0])
+    return leaving_variable_row
 
-    # Output results
-    optimal_variables = [augmented_matrix[i][n] for i in range(m)]
-    optimal_result = sum(c[i] * optimal_variables[i] for i in range(n))
+def update_tableau(entering_variable, leaving_variable):
+
+
+    basic_set.append(entering_variable)
+    basic_set.remove(leaving_variable)
+
+    B_matrix = []
+    for i in range(m):
+        arr=[]
+        for basic in basic_set:
+            arr.append(tableau[i][basic])
+        B_matrix.append(arr)
+
+    c_B = []
+    for basic in basic_set:
+        c_B.append(c[basic])
+
+    # tableu = inverse_of_B * tableau
+    inverse_B = modify_B(B_matrix)
+    for i in range(m):
+        for j in range(n):
+            tableau[i][j] = 0
+            for k in range(m):
+                tableau[i][j] += inverse_B[i][k] * B_matrix[k][j]
+
+
+
+def revised_simplex_algorithm():
+    while True:
+        
+        entering_variable, c = find_entering_variable()
+        if entering_variable is None:
+            break  # Optimal solution reached
+        leaving_variable = find_leaving_variable(tableau, entering_variable)
+        if leaving_variable is None:
+            print("Unbounded problem.")
+            return
+        update_tableau(entering_variable, leaving_variable)
+
+    optimal_solution = [0] * (len(tableau[0]) - 1)
+    for i in range(len(optimal_solution)):
+        column = [row[i] for row in tableau[1:]]
+        if column.count(0) == len(column) - 1 and column[-1] == 1:
+            pivot_row = column.index(1)
+            optimal_solution[i] = tableau[pivot_row + 1][-1]
+
+    optimal_result = sum(x * y for x, y in zip(c, optimal_solution))
 
     print("Hence, optimal solution is arrived with value of variables as :")
-    for i in range(n):
-        print(f"x{i + 1} = {optimal_variables[i]:.2f}, ", end="")
-    print()
-    print(f"Max Z = {optimal_result:.2f}")
+    for i, value in enumerate(optimal_solution):
+        print(f"x{i + 1} = {value}", end=", ")
+
+    print(f"\nMax Z = {optimal_result}")
+
 
 if __name__ == '__main__':
     for m in range(1, 4):
         print(f"Output for case {m}:")
         m, n, c, augmented_matrix = read_matrix(f"Assignment3_Data{m}.txt")
-        simplex_algorithm(m, n, c, augmented_matrix)
+        b = [row[-1] for row in augmented_matrix]
+        normal_matrix = [row[:-1] for row in augmented_matrix]
+        n = n+m
+
+
+        tableau = initialize_tableau(c, normal_matrix)
+  
+        basic_set = [f for f in range(n, n+m)]
+        B_matrix = []
+        for i in range(m):
+            arr=[]
+            for basic in basic_set:
+                arr.append(tableau[i][basic])
+            B_matrix.append(arr)
+
+        c_B = []
+        for basic in basic_set:
+            c_B.append(c[basic])
+        revised_simplex_algorithm()
         print("-" * 20)
+        break
